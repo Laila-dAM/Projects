960595e84732d418d72ff1344090ca028baf9c21
Laila-dAM <lailamota.238@gmail.com>
2025-01-23 13:55:24 -0300

    Minor fixes

--- Scripting_and_Automation/Bash/AutoBackup/scripts/backup_manager.sh
+++ Scripting_and_Automation/Bash/AutoBackup/scripts/backup_manager.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-LOG FILE="backup.log"
+LOG_FILE="backup.log"
 CONFIG_FILE="directories.conf"
 BACKUP_DIR="backups"
 FULL_BACKUP_DIR="$BACKUP_DIR/full"
 INCREMENTAL_BACKUP_DIR="$BACKUP_DIR/incremental"
 DATE=$(date +%Y-%m-%d_%H-%M-%S)
 
-function log_message{
+function log_message {
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> "$LOG_FILE"
 }
 
@@ -15,7 +15,7 @@ function perform_backup{
 mkdir -p "$FULL_BACKUP_DIR"
 mkdir -p "$INCREMENTAL_BACKUP_DIR"
 
-while read -r directorv; do
+while read -r directory; do
 if [ ! -d "$directory" ]; then
 log_message "Directory not found: $directory"
             continue
