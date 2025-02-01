82d3ca966a8c0e8b43e829e78e31fdd26bf3b711
Laila-dAM <lailamota.238@gmail.com>
2025-02-01 17:57:54 -0300

    Returns a kaomoji based on the action

---
+++ Codes-in-Java/Ascii Adventure/src/Kaomoji.java
@@ -0,0 +1,20 @@
+public class Kaomoji{
+    public static String getKaomoji(String action){
+        switch(action){
+                case "Attack":
+                return "(╯°□°）╯︵ ┻━┻";
+                case "Flee":
+                return "(ಥ﹏ಥ)";
+
+                case "Talk":
+                return "(｡♥‿♥｡)";
+
+                default:
+                return "(o_O)";
+ 
+
+
+
+        }
+    }
+}
