8a07581c83c4eb604aa83b23c2c2b0173f248785
Laila-dAM <lailamota.238@gmail.com>
2025-03-01 21:25:28 -0300

    Defines libraries and constants

---
+++ C_GameX/PuzzleFind 🧩/Detective/detetivedata.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_FILE_SIZE 1024
+#define ENCRYPTION_KEY 123
+#define FILENAME_ENCRYPTED "encrypted_file.dat"
+#define FILENAME_DECRYPTED "decrypted_file.txt"
+#define FILENAME_REVERSED "encoded_file.dat"
+
