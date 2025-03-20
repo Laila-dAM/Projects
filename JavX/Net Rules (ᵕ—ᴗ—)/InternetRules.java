e54fbdbf8a0248c0ca9b06e1d92cc62edfbfff5f
Laila-dAM <lailamota.238@gmail.com>
2025-03-19 22:32:07 -0300

    Shows internet rules according to the number entered.

---
+++ JavX/Net Rules (ᵕ—ᴗ—)/InternetRules.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class InternetRules {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("〜〜〜 Original Rules of the 〜〜〜");
+        System.out.println("      ✨  Internet  ✨");
+        System.out.println("  〜〜〜〜〜〜〜〜〜〜〜〜〜");
+        System.out.println("        𓁹‿𓁹");
+        System.out.println("  ✿ Which rule do you want to know? ✿");
+        System.out.println("  ✨ Enter a number (0 to 62): ✨");
+
+        int choice = scanner.nextInt();
