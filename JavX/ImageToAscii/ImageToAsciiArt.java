e22e401998fd2b92a07dcee09ae9371a025c6642
Seu Nome <seuemail@example.com>
2025-05-11 19:19:15 -0300

    Starts the application and displays the graphical interface

---
+++ JavX/ImageToAscii/ImageToAsciiArt.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.datatransfer.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class ImageToAsciiArt {
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(() -> new ImageToAsciiArt().createAndShowGUI());
+    }
+
+    private void createAndShowGUI(){
+        
+    }
+}
