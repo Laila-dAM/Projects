476d34fa23d8e5375ae5f2d79bfa8e676dae742f
Seu Nome <seuemail@example.com>
2025-05-27 19:38:10 -0300

    Adds initial setup, element drawing and ball collision logic.

--- JavX/JaVbit/Pong/pog.java
+++ JavX/JaVbit/Pong/pog.java
@@ -33,5 +33,42 @@ public class PongGame extends JPanel implements KeyListener, ActionListener {
 
     public PongGame(){
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(this);
 
+        timer = new Timer(10, this);
+        timer.start();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.WHITE);
+        for(int i = 0; i < HEIGHT; i += 15){
+            g.fillRect(WIDTH / 2 - 2, i, 4, 10);
+        }
+        g.fillRect(50, paddle1Y, PADDLE_WIDTH, PADDLE_HEIGHT);
+        g.fillRect(WIDTH - 50 - PADDLE_WIDTH, paddle2Y, PADDLE_WIDTH, PADDLE_HEIGHT);
+
+        g.fillOval(ballX, ballY, BALL_SIZE, BALL_SIZE);
+
+        g.setFont(new Font("Consolas", Font.BOLD, 36));
+        g.drawString(String.valueOf(score1), WIDTH / 4,50);
+        g.drawString(String.valueOf(score2), WIDTH * 3 / 4,50);
+        
+    }
+
+    private void moveBall(){
+        ballX += ballSpeedX;
+        ballY += ballSpeedY;
+
+        if (ballY <= 0 || ballY >= HEIGHT - BALL_SIZE){
+            ballSpeedY = -ballSpeedY;
+        }
+
+        if (ballX <= 50 + PADDLE_WIDTH && ballY + BALL_SIZE >= paddle1Y && ballY <= paddle1Y + PADDLE_HEIGHT){
+            
+        }
     }
