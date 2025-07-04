02a7f3ccbe839637ab38f051376f7d6cc3918c62
Laila-dAM <lailamota.238@gmail.com>
2025-07-04 18:55:27 -0300

    initialize building data and main game flow

--- JavX/TowerDef/tower.java
+++ JavX/TowerDef/tower.java
@@ -15,4 +15,29 @@ public class tower {
         "Wall", 20,
         "Farm", 25
     );
+    static Map<String, Integer> buildingDefense = Map.of(
+        "Tower", 15,
+        "Wall", 10,
+        "Farm", 0
+    );
+    static Map<String, Integer> buildingIncome = Map.of(
+        "Tower", 0,
+        "Wall", 0,
+        "Farm", 15
+    );
+
+    public static void main(String[] args) {
+        buildings.put("Tower", 0);
+        buildings.put("Wall", 0);
+        buildings.put("Farm", 0);
+        JOptionPane.showMessageDialog(null, "Welcome to City Defense! (｡•̀ᴗ-)✧\nBuild and defend your city!");
+
+        while(cityHealth > 0){
+            drawCity();
+            cityIncome();
+            playerBuild();
+            enemyAttack();
+            wave++;
+        }
+    }
 }
